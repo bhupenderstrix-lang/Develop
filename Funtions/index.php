601c405aa5db57555ee7bcf51a625b0b300ca1ce
Bhupender Chauhan <bhupender.strix@gmail.com>
2025-10-06 09:56:09 +0530

    Stage-One

---
+++ Funtions/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Project: The one
+ * Date: 01/10/2025
+ * Author:
+ */
+$heading = "<h1>THE ONE</h1>";
+$_1para = "<p>The one is a story<p>";
+
+echo $heading . ' ' . $_1para;
+
+?>
+
+
+<?php 
+$a= 45;
+$b= 1.6;
+$c= '12';
+$d= true;
+$e = $a +$b +$c +$d;
+echo $e;
+
+?>
+
+<?php
+echo "<p>The one is a story of a valour person.</p>";
+echo "<h4>Ending</h4>";
+?>
+ 
+ <?php
+$a= 12.5;
+$b= 55;
+$c= $a * $b;
+echo $c; 
+ ?>
---
+++ Index/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /**
+     * thi is multi line 
+     * comment format
+     */
+    // single line comment system
+    # this is also single line comment
+    ?>
+    
+    <?php
+    echo "<h1>The One</h1>";
+    ?>
+</body>
+</html>
